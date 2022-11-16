bdc28af7c6ee4ea060910a93244908f7071441a6
Cole Winchester <cowinch@gmail.com>
2022-11-16 17:35:00 -0600

    puppeteer web scraping working

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const request = require('request-promise')
+const axios = require('axios')
+const puppeteer = require('puppeteer')
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 
-app.get('/api', (req, res) => {
-    res.send('Hello world!')
+app.get('/products/:productId', (req, res) => {
+    const { productId } = req.params
+
+    async function grabProduct(productId) {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.goto(`https://www.amazon.com/products/dp/${productId}/`)
+        const data = {
+            title: "",
+            price: ''
+        }
+        data.title = await page.$eval("#productTitle", element => element.textContent)
+        console.log(data.title)
+
+        data.price = await page.$eval(".a-price .a-offscreen", element => element.textContent)
+        console.log(data.price)
+        res.send(data)
+        await browser.close()
+    }
+    grabProduct(productId)
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))

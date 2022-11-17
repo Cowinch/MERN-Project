3130ccfa5cc462659941600a123cc654b9ba0d07
Cole Winchester <cowinch@gmail.com>
2022-11-17 00:45:11 -0600

    attempt to fix heroku bug

--- index.js
+++ index.js
@@ -8,35 +8,36 @@ app.use(express.json())
 app.get('/', (req, res) => res.send("Welcome to the default landing page. Use /products/<product id> to choose a produc to scrape"))
 
 app.get('/products/:productId', (req, res) => {
-    const { productId } = req.params
+  const { productId } = req.params
 
-    async function grabProduct(productId) {
-        const browser = await puppeteer.launch({
-            'args' : [
-              '--no-sandbox',
-              '--disable-setuid-sandbox'
-            ]
-          });
-        const page = await browser.newPage()
-        await page.goto(`https://www.amazon.com/products/dp/${productId}/`)
-        const data = {
-            title: "",
-            price: '',
-            reviews: '',
-            reviewRating: '',
-            image: '',
-            url: `https://www.amazon.com/products/dp/${productId}/`
-        }
-        data.title = await page.$eval("#productTitle", element => element.textContent)
-
-        data.price = await page.$eval(".a-price .a-offscreen", element => element.textContent)
-        data.reviews = await page.$eval("#acrCustomerReviewText", element => element.textContent)
-        data.reviewRating = await page.$eval("#averageCustomerReviews .a-icon-alt", element => element.textContent)
-        data.image = await page.$eval("#landingImage", element => element.src)
-        res.send(data)
-        await browser.close()
+  async function grabProduct(productId) {
+    const browser = await puppeteer.launch({
+      headless: true,
+      'args': [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ]
+    });
+    const page = await browser.newPage()
+    await page.goto(`https://www.amazon.com/products/dp/${productId}/`)
+    const data = {
+      title: "",
+      price: '',
+      reviews: '',
+      reviewRating: '',
+      image: '',
+      url: `https://www.amazon.com/products/dp/${productId}/`
     }
-    grabProduct(productId)
+    data.title = await page.$eval("#productTitle", element => element.textContent)
+
+    data.price = await page.$eval(".a-price .a-offscreen", element => element.textContent)
+    data.reviews = await page.$eval("#acrCustomerReviewText", element => element.textContent)
+    data.reviewRating = await page.$eval("#averageCustomerReviews .a-icon-alt", element => element.textContent)
+    data.image = await page.$eval("#landingImage", element => element.src)
+    res.send(data)
+    await browser.close()
+  }
+  grabProduct(productId)
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
